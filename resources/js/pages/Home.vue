c6b3e0c4dc368670af735e631d722acc9256acf3
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-05-28 17:35:54 +0300

    publish lang folder

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
 import { Head } from '@inertiajs/vue3';
+
+import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
 </script>
 
 <template>
@@ -9,14 +11,18 @@ import { Head } from '@inertiajs/vue3';
     <AppLayout>
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                <div class="border-muted relative aspect-video overflow-hidden rounded-xl border">
+                    <PlaceholderPattern />
                 </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                <div class="border-muted relative aspect-video overflow-hidden rounded-xl border">
+                    <PlaceholderPattern />
                 </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                <div class="border-muted relative aspect-video overflow-hidden rounded-xl border">
+                    <PlaceholderPattern />
                 </div>
             </div>
-            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
+            <div class="border-muted relative min-h-[100vh] flex-1 rounded-xl border md:min-h-min">
+                <PlaceholderPattern />
             </div>
         </div>
     </AppLayout>
