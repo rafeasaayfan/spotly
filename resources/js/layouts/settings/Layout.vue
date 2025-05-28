80e48625d33471dbf5eeda4bc47f3e61723843a9
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-05-28 15:08:11 +0300

    fix the setting responsive design

--- resources/js/layouts/settings/Layout.vue
+++ resources/js/layouts/settings/Layout.vue
@@ -30,14 +30,14 @@ const currentPath = page.props.ziggy?.location ? new URL(page.props.ziggy.locati
 </script>
 
 <template>
-    <div class="flex flex-col gap-3 px-4 py-6 h-screen">
+    <div class="flex flex-col gap-3 px-4 py-6 lg:h-screen">
         <header class="w-full bg-card rounded-md p-4">
             <Heading title="Settings" description="Manage your profile and account settings" />
         </header>
 
-        <div class="grid grid-cols-5 space-y-8 md:space-y-0 lg:space-x-8 lg:space-y-0 h-full">
+        <div class="grid grid-cols-7 lg:grid-cols-5 gap-5 h-full">
 
-            <aside class="col-span-1 w-full max-w-xl lg:w-48 bg-navs py-4 px-2 rounded-md h-full">
+            <aside class="col-span-7 md:col-span-2 lg:col-span-1 w-full bg-navs py-4 px-2 rounded-md h-full">
                 <nav class="flex flex-col space-x-0 space-y-2">
                     <Button
                         v-for="item in sidebarNavItems"
@@ -53,10 +53,10 @@ const currentPath = page.props.ziggy?.location ? new URL(page.props.ziggy.locati
                 </nav>
             </aside>
 
-            <Separator class="my-6 md:hidden" />
+            <Separator class="col-span-3 my-2 md:hidden" />
 
-            <div class="col-span-4 w-full p-4 rounded-md">
-                <section class="max-w-xl space-y-12">
+            <div class="col-span-7 md:col-span-5 lg:col-span-4 w-full p-4 rounded-md flex md:justify-center">
+                <section class="w-full space-y-12">
                     <slot />
                 </section>
             </div>
