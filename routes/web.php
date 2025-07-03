d66d3f5550b9d4baec6f9ee6c570236ed9e5f7ab
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-07-03 15:35:15 +0300

    insert websites routes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\WebsiteBuilderController;
 use App\Http\Middleware\HandleLanguage;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/setLang/{lang}', function ($lang = null) {
     App::setLocale($lang);
@@ -25,4 +24,5 @@ Route::middleware([HandleLanguage::class])->group(function () {
     require __DIR__ . '/dashboard.php';
     require __DIR__ . '/settings.php';
     require __DIR__ . '/auth.php';
+    require __DIR__ . '/webistes.php';
 });
