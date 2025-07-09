22ccc94eeaa9da4644139794f41a68dae5590a5c
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-07-10 01:45:13 +0300

    fix the rule

--- app/Http/Requests/Dashboard/Pages/WebsiteUsers/StoreWebsiteUserRequest.php
+++ app/Http/Requests/Dashboard/Pages/WebsiteUsers/StoreWebsiteUserRequest.php
@@ -22,14 +22,14 @@ class StoreWebsiteUserRequest extends FormRequest
                 'email',
                 'max:255',
                 'unique:website_users,email',
-                Rule::unique('website_users', 'email')->where('website_id', $this->website_id)
+                Rule::unique('website_users', 'email')->where('website_id', $this->input('website_id'))
             ],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'phone_number' => [
                 'nullable',
                 'string',
                 'max:255',
-                Rule::unique('website_users', 'phone_number')->where('website_id', $this->website_id)
+                Rule::unique('website_users', 'phone_number')->where('website_id', $this->input('website_id'))
             ],
             'status' => ['required', 'in:active,inactive,banned'],
             'email_verified_at' => ['nullable', 'date'],
--- app/Http/Requests/Dashboard/Pages/WebsiteUsers/UpdateWebsiteUserRequest.php
+++ app/Http/Requests/Dashboard/Pages/WebsiteUsers/UpdateWebsiteUserRequest.php
@@ -22,14 +22,16 @@ class UpdateWebsiteUserRequest extends FormRequest
                 'email',
                 'max:255',
                 'unique:website_users,email',
-                Rule::unique('website_users', 'email')->where('website_id', $this->website_id)->ignore($this->route('websiteUser')->id)
+                Rule::unique('website_users', 'email')->where('website_id', $this->input('website_id'))
+                ->ignore($this->route('websiteUser')->id)
             ],
             'password' => ['nullable', 'string', 'min:8', 'confirmed'],
             'phone_number' => [
                 'nullable',
                 'string',
                 'max:255',
-                Rule::unique('website_users', 'phone_number')->where('website_id', $this->website_id)->ignore($this->route('websiteUser')->id)
+                Rule::unique('website_users', 'phone_number')->where('website_id', $this->input('website_id'))
+                ->ignore($this->route('websiteUser')->id)
             ],
             'status' => ['required', 'in:active,inactive,banned'],
             'email_verified_at' => ['nullable', 'date'],
