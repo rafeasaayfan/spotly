0ecaf83becb5743a26ffa6b5621ccae0c56fd25d
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-07-20 05:10:17 +0300

    heading small page classes

--- resources/js/components/headers/HeadingSmall.vue
+++ resources/js/components/headers/HeadingSmall.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
+import { cn } from '@/lib/utils';
+
 interface Props {
     title: string;
     description?: string;
+    class?: string;
+    titleClass?: string;
+    descriptionClass?: string;
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
 </script>
 
 <template>
-    <header class="w-fit ps-2 border-s border-muted">
-        <h3 class="text-active font-medium">{{ title }}</h3>
-        <p v-if="description" class="text-sm text-body-muted opacity-70">
+    <header :class="cn('w-fit ps-2 border-s border-muted', props.class)">
+        <h3 :class="cn('text-active font-medium', props.titleClass)">{{ title }}</h3>
+        <p v-if="description" :class="cn('text-sm text-body-muted opacity-70', props.descriptionClass)">
             {{ description }}
         </p>
     </header>
