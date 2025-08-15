e7271f14228d95d84caff24ae447a2de57084b2b
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-08-16 01:46:59 +0300

    add a multi input field

---
+++ resources/js/components/ui/fields/MultiInput.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+import { useVModel } from '@vueuse/core'
+import { Button } from '../button';
+import { Plus, X } from 'lucide-vue-next';
+import { Input } from '.';
+
+const props = defineProps<{
+  defaultValue?: (string | number)[]
+  modelValue: (string | number)[]
+  class?: HTMLAttributes['class']
+  placeholder?: string
+}>()
+
+const emits = defineEmits<{
+  (e: 'update:modelValue', payload: (string | number)[]): void
+}>()
+
+// make sure we start with an array
+const modelValue = useVModel(props, 'modelValue', emits, {
+  passive: true,
+  defaultValue: props.defaultValue ?? [''],
+})
+
+// add new empty field
+function addField() {
+  modelValue.value.push('')
+}
+
+// remove a field
+function removeField(index: number) {
+  modelValue.value.splice(index, 1)
+}
+</script>
+
+<template>
+  <div class="space-y-2">
+    <div
+      v-for="(val, index) in modelValue"
+      :key="index"
+      class="flex items-center gap-2"
+    >
+      <Input
+        v-model="modelValue[index]"
+        :placeholder="props.placeholder"
+      />
+
+      <div>
+        <Button
+          type="button"
+          size="icon"
+          variant="destructive"
+          class="rounded-full size-7 bg-[var(--destructive)]/50 dark:bg-[var(--destructive)]/30 
+          hover:bg-[var(--destructive)] dark:hover:bg-[var(--destructive)] hover:scale-100 hover:translate-0"
+          @click="removeField(index)"
+          v-if="modelValue.length > 1"
+        >
+          <X class="size-3" />
+        </Button>
+      </div>
+    </div>
+
+    <Button
+      type="button"
+      size="sm"
+      variant="ghost"
+      @click="addField"
+    >
+      <Plus class="size-3.5" />
+      <span>Add</span>
+    </button>
+  </div>
+</template>
