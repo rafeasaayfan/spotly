a5d5752bac6ae6b5d947c8583f69974eae3457b2
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-08-15 15:18:29 +0300

    ordering the websites pages

---
+++ resources/js/pages/websites/e-commerce/dashboard/Dashboard.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import DashboardLayout from '@/layouts/DashboardLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
+import { useSidebar } from '@/components/ui/sidebar/utils';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: '/dashboard',
+    },
+];
+
+const { dashboardFor } = useSidebar();
+
+dashboardFor.value = 'e-commerce';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <DashboardLayout :breadcrumbs="breadcrumbs">
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-muted">
+                    <PlaceholderPattern />
+                </div>
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-muted">
+                    <PlaceholderPattern />
+                </div>
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-muted">
+                    <PlaceholderPattern />
+                </div>
+            </div>
+            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-muted md:min-h-min">
+                <PlaceholderPattern />
+            </div>
+        </div>
+    </DashboardLayout>
+</template>
