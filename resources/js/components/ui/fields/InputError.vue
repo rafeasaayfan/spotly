5906d2266c6211a16679765155ac6bce5c81983d
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-08-24 19:25:19 +0300

    Enhance InputError component by allowing custom class prop and applying utility class for dynamic styling

--- resources/js/components/ui/fields/InputError.vue
+++ resources/js/components/ui/fields/InputError.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-defineProps<{
+import { cn } from '@/lib/utils';
+
+const props = defineProps<{
+    class?: string;
     message?: string;
 }>();
 </script>
 
 <template>
     <div v-show="message">
-        <p class="text-sm text-active-link-2">
+        <p :class="cn('text-sm text-active-link-2', props.class)">
             {{ message }}
         </p>
     </div>
