16a723edc4a85d5017d6281104606b529c176e3e
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-08-24 19:21:46 +0300

    Add WebsitePolicy for managing user permissions on Website models; integrate policy with Gate in AppServiceProvider

---
+++ app/Policies/WebsitePolicy.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Website;
+use Illuminate\Auth\Access\Response;
+
+class WebsitePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Website $website): bool
+    {
+        if ($user->id === $website->owner_id) {
+            return true;
+        }
+
+        if (in_array($user->role, ['admin', 'super_admin'])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Website $website): bool
+    {
+        if ($user->id === $website->owner_id) {
+            return true;
+        }
+
+        if (in_array($user->role, ['admin', 'super_admin'])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Website $website): bool
+    {
+        if ($user->id === $website->owner_id) {
+            return true;
+        }
+
+        if (in_array($user->role, ['admin', 'super_admin'])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Website $website): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Website $website): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine if the user can activate/deactivate the website.
+     */
+    public function activate(User $user, Website $website)
+    {
+        if ($user->id === $website->owner_id) {
+            return true;
+        }
+
+        if (in_array($user->role, ['admin', 'super_admin'])) {
+            return true;
+        }
+
+        return false;
+    }
+}
