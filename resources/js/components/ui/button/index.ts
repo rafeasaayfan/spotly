1c30b94e1ee829c94291ad6a4df2772d3cdfb6bc
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-09-01 01:00:59 +0300

    fix the responsive size

--- resources/js/components/ui/button/index.ts
+++ resources/js/components/ui/button/index.ts
@@ -3,18 +3,18 @@ import { cva, type VariantProps } from 'class-variance-authority'
 export { default as Button } from './Button.vue'
 
 export const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm cursor-pointer font-medium transition-all duration-300 ease-in-out hover:-translate-y-0.5 hover:scale-98 active:scale-96 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*=\'size-\'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-xs sm:text-sm cursor-pointer font-medium transition-all duration-300 ease-in-out hover:-translate-y-0.5 hover:scale-98 active:scale-96 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*=\'size-\'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
   {
     variants: {
       variant: {
         default:
-          'bg-primary text-for-bg-primary shadow-xs',
+          'bg-primary text-for-bg-primary',
         destructive:
-          'bg-destructive text-for-bg-destructive shadow-xs',
+          'bg-destructive text-for-bg-destructive',
         outline:
-          'border bg-content-3 shadow-xs text-body border-muted',
+          'border bg-content-3 text-body border-muted',
         secondary:
-          'bg-secondary text-for-bg-secondary shadow-xs',
+          'bg-secondary text-for-bg-secondary',
         ghost:
           'bg-content text-body-muted',
         tab:
@@ -22,7 +22,7 @@ export const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
+        default: 'h-8 px-3 py-1.5 sm:h-9 sm:px-4 sm:py-2 has-[>svg]:px-3',
         sm: 'h-8 rounded-md gap-1 px-3 has-[>svg]:px-2.5 text-xs',
         lg: 'h-10 rounded-md px-6 has-[>svg]:px-4 text-base',
         icon: 'size-9',
