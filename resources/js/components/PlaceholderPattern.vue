5d56bb57bbad74d5d1702a490b0697ba806c3b4e
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-09-01 00:50:56 +0300

    making it with the cn

--- resources/js/components/PlaceholderPattern.vue
+++ resources/js/components/PlaceholderPattern.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import { computed } from 'vue';
+import { cn } from '@/lib/utils'
+import type { HTMLAttributes } from 'vue';
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
 
 const patternId = computed(() => `pattern-${Math.random().toString(36).substring(2, 9)}`);
 </script>
 
 <template>
-    <svg class="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" fill="none">
+    <svg :class="cn('absolute inset-0 z-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20 opacity-50', props.class)" fill="none">
         <defs>
             <pattern :id="patternId" x="0" y="0" width="8" height="8" patternUnits="userSpaceOnUse">
                 <path d="M-1 5L5 -1M3 9L8.5 3.5" stroke-width="0.5"></path>
