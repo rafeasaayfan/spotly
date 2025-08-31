6c4a64c621ed047e70ad39c6608a25d652ce60bc
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-09-01 00:50:23 +0300

    create the ui service

---
+++ app/Services/UiService.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\TemplateColor;
+use App\Models\WebsiteTemplate;
+
+class UiService
+{
+    protected int $websiteId;
+    protected int $template_color_id;
+    protected int $template_id;
+    protected array $template_images;
+    protected bool $is_custom;
+    protected array $colors;
+
+    /**
+     * Create a new UiService instance.
+     *
+     * @param int   $websiteId         The website ID to associate the template with.
+     * @param int   $template_color_id The template color ID.
+     * @param int   $template_id       The template ID.
+     * @param array $template_images   The template images.
+     * @param bool  $is_custom         Whether the template uses custom colors.
+     * @param array $colors            The custom colors (if any).
+     */
+    public function __construct(int $websiteId, int $template_color_id, int $template_id, array $template_images, bool $is_custom = false, array $colors = [])
+    {
+        $this->websiteId = $websiteId;
+        $this->template_color_id = $template_color_id;
+        $this->template_id = $template_id;
+        $this->template_images = $template_images;
+        $this->is_custom = $is_custom;
+        $this->colors = $colors;
+    }
+
+    /**
+     * Store a new website template.
+     *
+     * If custom colors are provided, create a new TemplateColor and use its ID.
+     * Otherwise, use the provided template_color_id and template_images.
+     *
+     * @return void
+     */
+    public function storeTemplate(bool $is_active = false): string | bool
+    {
+        $checkDuplicateTemplate = WebsiteTemplate::where('website_id', $this->websiteId)
+        ->where('template_id', $this->template_id)->where('template_color_id', $this->template_color_id)
+        ->where('is_custom', false)->first();
+        if($checkDuplicateTemplate) {
+            return 'You already have this template!';
+        }
+
+        $websiteTemplateCount = WebsiteTemplate::where('website_id', $this->websiteId)->count();
+        if ($websiteTemplateCount === 4) {
+            return 'You can\'t create more then four templates!';
+        }
+
+        $templateColorId = '';
+        $template_images = [''];
+
+        if (!empty($this->is_custom) && $this->is_custom && !empty($this->colors)) {
+            $templateColors = TemplateColor::create([
+                ...$this->colors,
+                'is_custom' => $this->is_custom,
+            ]);
+            $templateColorId = $templateColors->id;
+        } else {
+            $templateColorId = $this->template_color_id;
+            $template_images = $this->template_images;
+        }
+
+        WebsiteTemplate::create([
+            'website_id' => $this->websiteId,
+            'template_id' => $this->template_id,
+            'template_color_id' => $templateColorId,
+            'template_images' => $template_images,
+            'is_custom' => $this->is_custom,
+            'is_active' => $is_active
+        ]);
+
+        return true;
+    }
+}
