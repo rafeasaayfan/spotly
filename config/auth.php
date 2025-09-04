ad56b19e3bc1a3b9508851a04da3c51d46d60613
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-09-05 02:16:55 +0300

    add a new guard for the websites

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'website' => [
+            'driver' => 'session',
+            'provider' => 'website_users',
+        ],
     ],
 
     /*
@@ -63,6 +68,11 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => env('AUTH_MODEL', App\Models\User::class),
+        ],
+
+        'website_users' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\WebsiteUser::class,
         ],
 
         // 'users' => [
@@ -93,6 +103,13 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
+            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+
+        'website_users' => [
+            'provider' => 'website_users',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
