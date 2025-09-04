82e1f388fba8db2e20e662f562afb8c57e672b2f
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-09-05 02:41:17 +0300

    fix the e-commerce routes and add the auth websites routes

--- routes/websites/e-commerce/dashboard.php
+++ routes/websites/e-commerce/dashboard.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\Websites\ECommerce\Dashboard\DashboardController;
+use App\Http\Controllers\Websites\Ecommerce\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'verified'])->prefix('dashboard')->name('e-commerce.dashboard.')->group(function () {
+Route::prefix('dashboard')->name('dashboard.')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('index');
 });
--- routes/websites/e-commerce/web.php
+++ routes/websites/e-commerce/web.php
@@ -3,14 +3,16 @@
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
+Route::name('e-commerce.')->group(function () {
+    Route::get('/', function () {
+        return Inertia::render('websites/e-commerce/templates/spotly-ui/pages/home/Home');
+    })->name('home');
 
-Route::get('/e-commerce', function() {
-    return Inertia::render('websites/e-commerce/templates/spotly-ui/pages/home/Home');
+    Route::get('/shop', function () {
+        return Inertia::render('websites/e-commerce/templates/spotly-ui/pages/shop/Shop');
+    })->name('shop');
+
+    Route::middleware(['auth:website', 'verified:website.verification.notice'])->group(function () {
+        require __DIR__ . '/dashboard.php';
+    });
 });
-
-Route::get('/shop', function() {
-    return Inertia::render('websites/e-commerce/spotly-ui/pages/shop/Shop');
-});
-
-
-require __DIR__ . '/dashboard.php';
