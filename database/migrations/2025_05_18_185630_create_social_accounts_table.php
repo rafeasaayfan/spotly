1e23cbb059d85d52f28a085f0b26976b711e5ddf
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-09-05 02:18:14 +0300

    Update social_accounts migration to specify 'users' table constraint for user_id foreign key

--- database/migrations/2025_05_18_185630_create_social_accounts_table.php
+++ database/migrations/2025_05_18_185630_create_social_accounts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('social_accounts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('provider'); // e.g., "google"
             $table->string('provider_id')->unique(); // Google ID
             $table->text('token')->nullable();
