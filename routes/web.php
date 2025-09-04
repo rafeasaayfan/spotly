743fb8656762d0d2ba2834111fb8ac37695720fa
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-09-05 02:40:39 +0300

    Refactor web routes to group Spotly routes under a specific domain and introduce IdentifyWebsite middleware for improved organization and clarity.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Spotly\LandingController;
 use App\Http\Controllers\Spotly\WebsiteBuilderController;
 use App\Http\Controllers\Spotly\WebsitePreviewController;
 use App\Http\Middleware\HandleLanguage;
+use App\Http\Middleware\IdentifyWebsite;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
@@ -17,28 +18,35 @@ Route::get('/setLang/{lang}', function ($lang = null) {
 
 Route::middleware([HandleLanguage::class])->group(function () {
 
-    Route::get('/', [LandingController::class, 'index'])->name('landing');
-    Route::put('/subscribe', [LandingController::class, 'subscribe'])->name('subscribe');
-    Route::put('/contactUs', [LandingController::class, 'contactUs'])->name('contactMessages');
+    // Spotly Routes
+    Route::domain('http://127.0.0.1')->group(function () {
+        Route::get('/', [LandingController::class, 'index'])->name('landing');
+        Route::put('/subscribe', [LandingController::class, 'subscribe'])->name('subscribe');
+        Route::put('/contactUs', [LandingController::class, 'contactUs'])->name('contactMessages');
+    
+        // WebsiteBuilder
+        Route::prefix('websiteBuilder')->name('websiteBuilder.')->group(function() {
+            Route::get('/', [WebsiteBuilderController::class, 'index'])->name('index');
+            Route::post('/wizard/{step}', [WebsiteBuilderController::class, 'wizard'])->name('wizard');
+    
+            // UI 
+            Route::post('/getTemplateTemplateColors', [WebsiteBuilderController::class, 'getTemplateTemplateColors'])->name('getTemplateTemplateColors');
+            Route::post('/customColors', [WebsiteBuilderController::class, 'customColors'])->name('customColors');
+        });
+    
+        // Preview
+        Route::post('/websites/preview', [WebsitePreviewController::class, 'preview'])->name('website.preview');
 
-    // WebsiteBuilder
-    Route::prefix('websiteBuilder')->name('websiteBuilder.')->group(function() {
-        Route::get('/', [WebsiteBuilderController::class, 'index'])->name('index');
-        Route::post('/wizard/{step}', [WebsiteBuilderController::class, 'wizard'])->name('wizard');
-
-        // UI 
-        Route::post('/getTemplateTemplateColors', [WebsiteBuilderController::class, 'getTemplateTemplateColors'])->name('getTemplateTemplateColors');
-        Route::post('/customColors', [WebsiteBuilderController::class, 'customColors'])->name('customColors');
+        require __DIR__ . '/dashboard.php';
+        require __DIR__ . '/client.php';
+        require __DIR__ . '/settings.php';
+        require __DIR__ . '/auth.php';
     });
 
-    // Preview
-    Route::post('/websites/preview', [WebsitePreviewController::class, 'preview'])->name('website.preview');
-
-    require __DIR__ . '/dashboard.php';
-    require __DIR__ . '/client.php';
-    require __DIR__ . '/settings.php';
-    require __DIR__ . '/auth.php';
-    require __DIR__ . '/websites/e-commerce/web.php';
+    // Websites Routes
+    Route::middleware([IdentifyWebsite::class])->name('website.')->group(function () {
+        require __DIR__ . '/websites/main.php';
+    });
 });
 
 // Dashboards Routes function
