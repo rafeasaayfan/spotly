3966d4d97b80df8e25a1b226f902cf0cbde09547
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-09-05 02:25:54 +0300

    Remove unused dashboardFor prop from sidebar context to streamline component functionality

--- resources/js/components/ui/sidebar/utils.ts
+++ resources/js/components/ui/sidebar/utils.ts
@@ -16,5 +16,4 @@ export const [useSidebar, provideSidebarContext] = createContext<{
   openMobile: Ref<boolean>
   setOpenMobile: (value: boolean) => void
   toggleSidebar: () => void
-  dashboardFor: Ref<string>
 }>('Sidebar')
