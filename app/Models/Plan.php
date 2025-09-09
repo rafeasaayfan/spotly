afc71e29f6e2f90973465511cc53d75dd9d9bce7
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-09-09 04:19:04 +0300

    created plan migration and model

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -6,9 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    protected $fillable = ['name', 'price', 'currency', 'duration', 'features', 'is_active'];
+    protected $fillable = ['website_type_id', 'name', 'price', 'currency', 'duration', 'features', 'is_active'];
     
     protected $casts = [
         'features' => 'array'
     ];
+
+    /**
+     * The plan has website type.
+     */
+    public function websiteType()
+    {
+        return $this->belongsTo(WebsiteType::class, 'website_type_id');
+    }
+
+    /**
+     * Get the active plan.
+     */
+    public function scopeActive($query)
+    {
+        return $query->where('is_active', 1);
+    }
 }
--- database/migrations/2025_06_11_195946_create_plans_table.php
+++ database/migrations/2025_06_11_195946_create_plans_table.php
@@ -13,13 +13,16 @@ return new class extends Migration
     {
         Schema::create('plans', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->foreignId('website_type_id')->nullable()->constrained('website_types')->onDelete('cascade');
+            $table->string('name');
             $table->decimal('price', 10, 2);
             $table->enum('currency', ['USD', 'LBP']);
             $table->enum('duration', ['monthly', 'yearly']);
             $table->json('features');
             $table->boolean('is_active')->default(true);
             $table->timestamps();
+
+            $table->unique(['website_type_id', 'name']);
         });
     }
 
