6234407eef5cdd12169568c3e8d761f762dda499
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-09-18 02:22:14 +0300

    change bg color

--- resources/js/components/navbar/AppNavbar.vue
+++ resources/js/components/navbar/AppNavbar.vue
@@ -40,7 +40,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <div :class="scrolled ? 'mt-0 bg-black/5 pt-0 backdrop-blur-lg dark:bg-white/5' : 'mt-2'" class="fixed z-20 w-full transition-all duration-300">
+    <div :class="scrolled ? 'mt-0 bg-black/3 pt-0 backdrop-blur-lg dark:bg-white/3' : 'mt-2'" class="fixed z-20 w-full transition-all duration-300">
         <div
             :class="scrolled ? 'px-2 md:px-4' : 'px-2 md:px-10'"
             class="mx-auto flex h-16 items-center justify-between transition-all duration-300 md:max-w-7xl"
