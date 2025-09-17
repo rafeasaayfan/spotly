31fac109a4e50dbb1e4b1f71a67fd61153e9992e
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-09-18 02:24:56 +0300

    refactor Radio component to include label prop and improve button design with icon integration

--- resources/js/components/ui/fields/Radio.vue
+++ resources/js/components/ui/fields/Radio.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { cn } from '@/lib/utils'
 import { useVModel } from '@vueuse/core'
+import { Check } from 'lucide-vue-next';
 import { computed, type HTMLAttributes } from 'vue'
+import Label from '../label/Label.vue';
 
 const props = defineProps<{
   modelValue: string | number | boolean
   value: string | number | boolean
   class?: HTMLAttributes['class']
+  label?: string
 }>()
 
 const emits = defineEmits<{
@@ -19,25 +22,21 @@ const isSelected = computed(() => model.value === props.value)
 </script>
 
 <template>
-  <button
-    type="button"
-    role="radio"
-    :aria-checked="isSelected"
-    @click="model = props.value"
-    :class="
-      cn(
-        'cursor-pointer rounded-full border border-blue-600/10 dark:border-blue-600/5 w-[19px] h-[19px] flex justify-center items-center transition-transform duration-150 active:scale-95 focus:outline-none focus:ring-0',
-        'bg-[var(--field)]',
-        isSelected
-          ? 'bg-blue-800 dark:bg-blue-600'
-          : 'bg-transparent',
-        props.class
-      )
-    "
-  >
-    <div
-      v-if="isSelected"
-      class="w-[9px] h-[9px] rounded-full bg-white"
-    ></div>
+  <button type="button" @click="model = props.value" class="flex items-center gap-1 cursor-pointer">
+    <div role="radio" :aria-checked="isSelected" :class="cn(
+      'cursor-pointer rounded-full border-2 border-muted size-[20px] flex justify-center items-center transition-transform duration-150 active:scale-95 focus:outline-none focus:ring-0',
+      'bg-[var(--field)]',
+      isSelected
+        ? 'bg-primary'
+        : 'bg-field',
+      props.class
+    )
+      ">
+      <div v-if="isSelected" class="w-full h-full rounded-full bg-primary flex items-center justify-center text-white">
+        <Check class="size-3.5" />
+      </div>
+    </div>
+
+    <Label>{{ props.label }}</Label>
   </button>
 </template>
