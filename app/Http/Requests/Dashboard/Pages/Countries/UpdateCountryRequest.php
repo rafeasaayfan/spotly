887b22504a35065d1a942ec5d1f441c4234568a2
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-09-22 03:43:18 +0300

    refactor validation rules in UpdateCountryRequest and UpdateTemplateTemplateColorRequest to use conditional validation for file uploads

--- app/Http/Requests/Dashboard/Pages/Countries/UpdateCountryRequest.php
+++ app/Http/Requests/Dashboard/Pages/Countries/UpdateCountryRequest.php
@@ -15,7 +15,7 @@ class UpdateCountryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'flag' => ['nullable', 'file', 'mimes:jpeg,png,jpg,gif,svg,webp', 'max:2048'],
+            'flag' => ['nullable', when('string', '', ['file', 'mimes:jpeg,png,jpg,gif,svg,webp', 'max:2048'])],
             'country' => [
                 'required',
                 'string',
--- app/Http/Requests/Dashboard/Pages/Ui/TemplateTemplateColors/UpdateTemplateTemplateColorRequest.php
+++ app/Http/Requests/Dashboard/Pages/Ui/TemplateTemplateColors/UpdateTemplateTemplateColorRequest.php
@@ -24,7 +24,7 @@ class UpdateTemplateTemplateColorRequest extends FormRequest
                     ->ignore($this->route('templateTemplateColor')->id)
             ],
             'is_default' => ['required', 'boolean'],
-            'uiImages.*' => ['nullable', 'file', 'mimes:jpeg,png,jpg,gif,webp,svg', 'max:5120'], // 5MB max
+            'uiImages.*' => ['nullable', when('string', '', ['file', 'mimes:jpeg,png,jpg,gif,svg,webp', 'max:5120'])],
         ];
     }
 }
