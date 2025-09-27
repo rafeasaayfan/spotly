9bc16ebe89129562de3c4558bb9b5341662928eb
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-09-28 02:48:39 +0300

    add the middlware for the websites role and remove the lang midd from here

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,7 @@
 
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
-use App\Http\Middleware\HandleLanguage;
+use App\Http\Middleware\HandleWebsiteRole;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -18,10 +18,13 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
-            HandleLanguage::class,
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
+        ]);
+
+        $middleware->alias([
+            'websiteRole' => HandleWebsiteRole::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
