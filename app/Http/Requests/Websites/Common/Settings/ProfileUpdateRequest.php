3075e71082c7678aa8e643ea4f89d84ce31f4e73
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-10-14 01:35:03 +0300

    ignore unique

---
+++ app/Http/Requests/Websites/Common/Settings/ProfileUpdateRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Websites\Common\Settings;
+
+use App\Models\WebsiteUser;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ProfileUpdateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => [
+                'required',
+                'string',
+                'lowercase',
+                'email',
+                'max:255',
+                Rule::unique(WebsiteUser::class)->ignore($this->user('website')->id),
+            ],
+            'phone_number' => [
+                'required',
+                'regex:/^(?:\+961)?(03\d{6}|70\d{6}|71\d{6}|76\d{6}|78\d{6}|79\d{6}|81\d{6})$/',
+                Rule::unique(WebsiteUser::class)->ignore($this->user('website')->id)
+            ],
+        ];
+    }
+}
