b41c389c69bdfdfc91cd762e16ae77fb63f0c5f2
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-10-14 01:33:00 +0300

    extend it with the base controller

--- app/Http/Controllers/Websites/Ecommerce/Dashboard/DashboardController.php
+++ app/Http/Controllers/Websites/Ecommerce/Dashboard/DashboardController.php
@@ -2,29 +2,16 @@
 
 namespace App\Http\Controllers\Websites\Ecommerce\Dashboard;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Websites\BaseController;
 // use Illuminate\Http\Request;
 use Inertia\Inertia;
 
-class DashboardController extends Controller
+class DashboardController extends BaseController
 {
-    public $website;
-
-    public function __construct()
-    {
-        $this->website = app('website')->load('media');
-    }
-
     public function index()
     {
-        $websiteNameAndLogo = [
-            'light_logo' => $this->website->light_logo,
-            'dark_logo' => $this->website->dark_logo,
-            'name' => $this->website->name,
-        ];
-
         return Inertia::render('websites/e-commerce/dashboard/Dashboard', [
-            'websiteNameAndLogo' => $websiteNameAndLogo
+            'websiteNameAndLogo' => $this->websiteNameAndLogo
         ]);
     }
 }
