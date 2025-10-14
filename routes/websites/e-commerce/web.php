d150f0555c8caa5564fd8647ebb6d90489a23fd5
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-10-14 04:00:19 +0300

    add the eco routes

--- routes/websites/e-commerce/web.php
+++ routes/websites/e-commerce/web.php
@@ -1,16 +1,22 @@
 <?php
 
-use Inertia\Inertia;
+use App\Http\Controllers\Websites\Ecommerce\CartController;
+use App\Http\Controllers\Websites\Ecommerce\HomeController;
+use App\Http\Controllers\Websites\Ecommerce\ProductController;
+use App\Http\Controllers\Websites\Ecommerce\ShopController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('e-commerce.')->group(function () {
-    Route::get('/', function () {
-        return Inertia::render('websites/e-commerce/templates/spotly-ui/pages/home/Home');
-    })->name('home');
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::put('/contactUs', [HomeController::class, 'contactUs'])->name('contactMessages');
 
-    Route::get('/shop', function () {
-        return Inertia::render('websites/e-commerce/templates/spotly-ui/pages/shop/Shop');
-    })->name('shop');
+    Route::get('/shop', [ShopController::class, 'index'])->name('shop');
+
+    Route::get('/product/{slug}', [ProductController::class, 'index'])->name('product');
+    Route::post('/addToCart', [ProductController::class, 'addToCart'])->name('addToCart');
+
+    Route::get('/cart', [CartController::class, 'index'])->name('cart');
+    Route::delete('/cart/removeItem', [CartController::class, 'removeItem'])->name('cart.removeItem');
 
     Route::middleware(['auth:website', 'verified:website.verification.notice', 'websiteUserRole:admin'])->group(function () {
         require __DIR__ . '/dashboard.php';
