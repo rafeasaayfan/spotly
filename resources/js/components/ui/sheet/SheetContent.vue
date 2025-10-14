357021dbf29f56e162ee7dd0dd57a4e3e45dc0a6
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-10-14 04:04:42 +0300

    remove unused close button and icon from SheetContent.vue; adjust padding in SheetHeader.vue

--- resources/js/components/ui/sheet/SheetContent.vue
+++ resources/js/components/ui/sheet/SheetContent.vue
@@ -2,7 +2,7 @@
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
 import { reactiveOmit } from '@vueuse/core'
-import { X } from 'lucide-vue-next'
+// import { X } from 'lucide-vue-next'
 import {
   DialogClose,
   DialogContent,
@@ -52,12 +52,12 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
     >
       <slot />
 
-      <DialogClose
+      <!-- <DialogClose
         class="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none"
       >
         <X class="size-4" />
         <span class="sr-only">Close</span>
-      </DialogClose>
+      </DialogClose> -->
     </DialogContent>
   </DialogPortal>
 </template>
