88ac8e9e46ad791863336c6efdca029dd7e1ac71
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-10-22 11:21:33 +0300

    enhance

--- app/Http/Controllers/Websites/Ecommerce/HomeController.php
+++ app/Http/Controllers/Websites/Ecommerce/HomeController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers\Websites\Ecommerce;
 
 use App\Http\Controllers\Websites\BaseController;
 use App\Models\Category;
-use App\Models\EcommerceCartItem;
 use App\Models\EcommerceProduct;
 use App\Models\WebsiteMessage;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class HomeController extends BaseController
 {
+    /**
+     * Display the home detail page.
+     */
     public function index()
     {
         $homeSpecialProducts = EcommerceProduct::active()->inHome()->special()->whereHas('inStockVariants')
@@ -23,30 +24,17 @@ class HomeController extends BaseController
         $categories = Category::active()->inHome()->where('website_id', $this->website->id)
             ->withCount('ecommerceProducts')->get();
 
-        $cartItemsCount = EcommerceCartItem::query();
-        if(Auth::check()) {
-            $cartItemsCount = $cartItemsCount->whereHas('cart', function ($q) {
-                $q->where('website_id', $this->website->id)
-                  ->where('website_user_id', Auth::id());
-            })->count();
-        } else {
-            $cartItemsCount = $cartItemsCount->whereHas('cart', function ($q) {
-                $q->where('website_id', $this->website->id)
-                  ->where('session_id', session()->getId());
-            })->count();
-        }
-
         return $this->inertiaRender(
             'pages/home/Home',
             [
-                'colors' => $this->websiteTemplate->templateColor,
-                'websiteNameAndLogo' => $this->websiteNameAndLogo,
-                'websiteFooterData' => $this->websiteFooterData,
+                'colors' => $this->websiteTemplate()->templateColor,
+                'websiteNameAndLogo' => $this->websiteNameAndLogo(),
+                'websiteFooterData' => $this->websiteFooterData(),
                 'homeSpecialProducts' => $homeSpecialProducts->isNotEmpty() ? $homeSpecialProducts : $homeProducts,
                 'homeProducts' => $homeProducts,
                 'categories' => $categories,
                 'aboutUs' => $this->website->about_us,
-                'cartItemsCount' => $cartItemsCount
+                'cartItemsCount' => $this->cartItems()?->count()
             ],
             true
         );
