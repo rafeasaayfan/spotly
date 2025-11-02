fe0432f8319edbebef28edf6fc622c8a7ec81798
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-11-02 20:19:10 +0200

    refactor UserMenuContent.vue to improve code readability and update websiteUserRole type to an array

--- resources/js/pages/websites/e-commerce/templates/spotly-ui/components/user/UserMenuContent.vue
+++ resources/js/pages/websites/e-commerce/templates/spotly-ui/components/user/UserMenuContent.vue
@@ -5,11 +5,11 @@ import UserInfo from './UserInfo.vue';
 import type { User } from '@/types';
 import { Link, router } from '@inertiajs/vue3';
 
-import { LogOut, Settings, LayoutGrid } from 'lucide-vue-next';
+import { LayoutGrid, LogOut, Settings } from 'lucide-vue-next';
 
 interface Props {
-    website_user: User,
-    websiteUserRole: string
+    website_user: User;
+    websiteUserRole: string[];
 }
 
 const handleLogout = () => {
@@ -20,7 +20,7 @@ defineProps<Props>();
 </script>
 
 <template>
-    <DropdownMenuLabel class="p-0 font-normal web-bg-content">
+    <DropdownMenuLabel class="web-bg-content p-0 font-normal">
         <div class="flex items-center gap-2 px-2 py-3 text-sm">
             <UserInfo :user="website_user" :show-email="true" />
         </div>
@@ -29,20 +29,22 @@ defineProps<Props>();
     <DropdownMenuSeparator class="bg-[var(--border_color_light)] dark:bg-[var(--border_color_dark)]" />
 
     <DropdownMenuGroup>
-        <DropdownMenuItem :as-child="true" class="bg-transparent hover:bg-[var(--bg_content_hover_light)] dark:hover:bg-[var(--bg_content_hover_dark)]
-            web-text-body"
+        <DropdownMenuItem
+            :as-child="true"
+            class="web-text-body bg-transparent hover:bg-[var(--bg_content_hover_light)] dark:hover:bg-[var(--bg_content_hover_dark)]"
         >
-            <Link class="flex w-full items-center gap-2 cursor-pointer" :href="route('website.profile.edit')" prefetch as="button">
+            <Link class="flex w-full cursor-pointer items-center gap-2" :href="route('website.profile.edit')" prefetch as="button">
                 <Settings class="h-4 w-4" />
                 {{ $t('settings') }}
             </Link>
         </DropdownMenuItem>
 
-
-        <DropdownMenuItem v-if="['admin', 'owner'].includes(websiteUserRole)" :as-child="true" class="bg-transparent 
-            hover:bg-[var(--bg_content_hover_light)] dark:hover:bg-[var(--bg_content_hover_dark)] web-text-body"
+        <DropdownMenuItem
+            v-if="websiteUserRole.includes('admin') || websiteUserRole.includes('owner')"
+            :as-child="true"
+            class="web-text-body bg-transparent hover:bg-[var(--bg_content_hover_light)] dark:hover:bg-[var(--bg_content_hover_dark)]"
         >
-            <Link class="flex w-full items-center gap-2 cursor-pointer" :href="route('website.e-commerce.dashboard.index')" prefetch as="button">
+            <Link class="flex w-full cursor-pointer items-center gap-2" :href="route('website.e-commerce.dashboard.index')" prefetch as="button">
                 <LayoutGrid class="h-4 w-4" />
                 {{ $t('dashboard') }}
             </Link>
@@ -52,7 +54,7 @@ defineProps<Props>();
     <DropdownMenuSeparator class="bg-[var(--border_color_light)] dark:bg-[var(--border_color_dark)]" />
 
     <DropdownMenuItem variant="destructive" :as-child="true" class="web-bg-danger web-text-for-danger">
-        <Link class="flex w-full items-center gap-2 cursor-pointer" method="post" :href="route('website.logout')" @click="handleLogout" as="button">
+        <Link class="flex w-full cursor-pointer items-center gap-2" method="post" :href="route('website.logout')" @click="handleLogout" as="button">
             <LogOut class="h-4 w-4" />
             {{ $t('auth.logout') }}
         </Link>
