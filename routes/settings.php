21125d41d3a3bc45a86a8eeeb7829c7418ea3a2c
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-11-07 18:25:58 +0200

    add the delete page

--- routes/settings.php
+++ routes/settings.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Spotly\Settings\PasswordController;
 use App\Http\Controllers\Spotly\Settings\ProfileController;
+use App\Http\Controllers\Spotly\Settings\DeleteAccountController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -10,10 +11,12 @@ Route::middleware('auth')->group(function () {
 
     Route::get('settings/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('settings/password', [PasswordController::class, 'edit'])->name('password.edit');
     Route::put('settings/password', [PasswordController::class, 'update'])->name('password.update');
+
+    Route::get('settings/delete-account', [DeleteAccountController::class, 'index'])->name('profile.destroy.index');
+    Route::delete('settings/delete-account', [DeleteAccountController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('settings/preferences', function () {
         return Inertia::render('settings/Preferences');
