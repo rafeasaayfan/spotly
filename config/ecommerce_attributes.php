5bdc2bdcddc542fbbe1914f1dc8c468373ae1a3d
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-11-22 15:15:12 +0200

    add ecommerce_attributes configuration file to define available product attributes for enhanced e-commerce functionality

---
+++ config/ecommerce_attributes.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Available Attribute Names
+    |--------------------------------------------------------------------------
+    */
+
+    'attributes' => [
+        ['value' => 'color', 'label' => 'Color'],
+        ['value' => 'size', 'label' => 'Size'],
+        ['value' => 'material', 'label' => 'Material'],
+        ['value' => 'weight', 'label' => 'Weight'],
+        ['value' => 'dimensions', 'label' => 'Dimensions'],
+        ['value' => 'height', 'label' => 'Height'],
+        ['value' => 'width', 'label' => 'Width'],
+        ['value' => 'model', 'label' => 'Model'],
+        ['value' => 'storage', 'label' => 'Storage'],
+        ['value' => 'ram', 'label' => 'RAM'],
+        ['value' => 'cpu', 'label' => 'CPU'],
+        ['value' => 'screen_size', 'label' => 'Screen Size'],
+        ['value' => 'collection', 'label' => 'Collection'],
+        ['value' => 'pattern', 'label' => 'Pattern'],
+        ['value' => 'style', 'label' => 'Style'],
+        ['value' => 'flavor', 'label' => 'Flavor'],
+        ['value' => 'package_size', 'label' => 'Package Size'],
+        ['value' => 'capacity', 'label' => 'Capacity'],
+        ['value' => 'volume', 'label' => 'Volume'],
+    ],
+
+];
