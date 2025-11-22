706189b20c4c66328ea8afe0f389fb389c79665c
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-11-22 15:16:50 +0200

    refactor ecommerce_product_variants migration to remove color attribute, add price attribute, and enhance data structure for improved product variant management

--- database/migrations/2025_06_26_000003_create_ecommerce_product_variants.php
+++ database/migrations/2025_06_26_000003_create_ecommerce_product_variants.php
@@ -14,12 +14,10 @@ return new class extends Migration
         Schema::create('ecommerce_product_variants', function (Blueprint $table) {
             $table->id();
             $table->foreignId('product_id')->constrained('ecommerce_products')->onDelete('cascade');
-            $table->string('color');
             $table->integer('stock_quantity')->default(0);
             $table->integer('reserved_quantity')->default(0);
+            $table->decimal('price', 10, 2);
             $table->timestamps();
-
-            $table->unique(['product_id', 'color']);
         });
     }
 
