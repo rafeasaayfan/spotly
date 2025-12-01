2bd41bb6c85609c95ea0ddcf1fed47c9a9c4fab6
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-12-01 18:23:03 +0200

    Enhance StatsOverviewCard component by adding a border for improved visual distinction

--- resources/js/components/dashboard/stats/StatsOverviewCard.vue
+++ resources/js/components/dashboard/stats/StatsOverviewCard.vue
@@ -5,7 +5,7 @@ const props = defineProps<{
 </script>
 
 <template>
-    <div class="rounded-md bg-[var(--content-3-bg-hover)] p-2">
+    <div class="rounded-md bg-[var(--content-3-bg-hover)] p-2 border border-muted">
         <div :class="['border-muted bg-body grid rounded-md border grid-cols-1 sm:grid-cols-2 lg:grid-cols-' + (props.gridSize ?? 4)]">
             <slot />
         </div>
