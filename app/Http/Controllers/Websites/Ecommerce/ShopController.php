6a3154e7f7c0062e565596217f8ac744ed848938
rafeaaSaayfan <rafehsaayfan@gmail.com>
2025-12-01 18:18:28 +0200

    Refactor ShopController to improve product retrieval and validation
    - Updated the `index` method to utilize `ShopRequest` for request validation, enhancing data integrity.
    - Replaced direct product retrieval logic with `ProductService`, streamlining the process of fetching shop products.
    - Removed the deprecated `getProducts` method, improving code clarity and maintainability.

--- app/Http/Controllers/Websites/Ecommerce/ShopController.php
+++ app/Http/Controllers/Websites/Ecommerce/ShopController.php
@@ -3,22 +3,25 @@
 namespace App\Http\Controllers\Websites\Ecommerce;
 
 use App\Http\Controllers\Websites\BaseController;
+use App\Http\Requests\Websites\Ecommerce\ShopRequest;
 use App\Models\Brand;
 use App\Models\Category;
-use App\Models\EcommerceProduct;
-use Illuminate\Http\Request;
+use App\Services\Websites\Ecommerce\ProductService;
 
 class ShopController extends BaseController
 {
     /**
      * Display the shop detail page.
      */
-    public function index(Request $request)
+    public function index(ShopRequest $request)
     {
+        $validated = $request->validated();
+
         $categories = Category::active()->where('website_id', $this->website->id)->get();
         $brands = Brand::active()->where('website_id', $this->website->id)->get();
 
-        $products = $this->getProducts($request);
+        $productService = new ProductService($this->website->id);
+        $products = $productService->getShopProducts($validated);
 
         return $this->inertiaRender(
             'pages/shop/Shop',
@@ -33,78 +36,5 @@ class ShopController extends BaseController
             ],
             true
         );
-    }
-
-    /**
-     * Get paginated products for the shop based on filters (search, category, brand, price, etc).
-     *
-     * @param Request $request
-     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
-     */
-    protected function getProducts(Request $request)
-    {
-        $query = EcommerceProduct::where('website_id', $this->website->id)->active()->whereHas('inStockVariants')
-            ->with(['category:id,name,ar_name', 'brand:id,name', 'variants.color']);
-
-        $validated = $request->validate([
-            'limit'     => 'nullable|integer|min:1|max:100',
-            'search'    => 'nullable|string|max:150',
-            'category'  => 'nullable|string|max:100',
-            'brand'     => 'nullable|string|max:100',
-            'minPrice'  => 'nullable|numeric|min:0',
-            'maxPrice'  => 'nullable|numeric|gt:minPrice|min:0',
-            'special'   => 'nullable|in:true,false',
-            'onSale'    => 'nullable|in:true,false',
-        ]);
-
-        $perPage  = (int) ($validated['limit'] ?? 12);
-        $search   = trim($validated['search'] ?? '');
-        $category = $validated['category'] ?? null;
-        $brand    = $validated['brand'] ?? null;
-        $minPrice = $validated['minPrice'] ?? 0;
-        $maxPrice = $validated['maxPrice'] ?? null;
-        $special  = $validated['special'] ?? false;
-        $onSale   = $validated['onSale'] ?? false;
-
-        if ($search) {
-            $query->where(function ($q) use ($search) {
-                $q->where('name', 'like', "%{$search}%")
-                    ->orWhere('short_description', 'like', "%{$search}%");
-            });
-        }
-
-        if ($category) {
-            $categoryId = Category::active()->where('website_id', $this->website->id)
-                ->where(function ($q) use ($category) {
-                    $q->where('name', $category)->orWhere('ar_name', $category);
-                })->value('id');;
-
-            if ($categoryId) $query->where('category_id', $categoryId);
-        }
-
-        if ($brand) {
-            $brandId = Brand::active()->where('website_id', $this->website->id)
-                ->where('name', $brand)->value('id');;
-
-            if ($brandId) $query->where('brand_id', $brandId);
-        }
-
-        if ($special) {
-            $query->where('is_special', true);
-        }
-
-        if ($onSale) {
-            $query->where('is_discount', true);
-        }
-
-        if ($minPrice !== null && $maxPrice !== null) {
-            $query->whereBetween('price', [$minPrice, $maxPrice]);
-        } elseif ($minPrice !== null) {
-            $query->where('price', '>=', $minPrice);
-        } elseif ($maxPrice !== null) {
-            $query->where('price', '<=', $maxPrice);
-        }
-
-        return $query->paginate($perPage);
     }
 }
