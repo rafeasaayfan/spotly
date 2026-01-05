77dd81bf9e94f34222c24cad13a8f98ab1a5c32e
rafeaaSaayfan <rafehsaayfan@gmail.com>
2026-01-05 20:54:47 +0200

    Add email verification routes for website management
    - Introduced new routes for verifying website email, including sending OTP and checking the last OTP, enhancing the email verification process.
    - Updated existing routes for website destruction to follow a more consistent naming convention, improving clarity and maintainability.
    - Refactored UI route for toggling active status to use hyphens for better readability.

--- routes/client.php
+++ routes/client.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Spotly\Client\Websites\Actions\EditWebsiteController;
 use App\Http\Controllers\Spotly\Client\Websites\Actions\UiWebsiteController;
 use App\Http\Controllers\Spotly\Client\Websites\Actions\ViewWebsiteController;
 use App\Http\Controllers\Spotly\Client\DashboardController;
+use App\Http\Controllers\Spotly\Client\Websites\Actions\VerifyWebsiteEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->prefix('dashboard/')->name('client.')->group(function () {
@@ -20,14 +21,19 @@ Route::middleware(['auth', 'verified'])->prefix('dashboard/')->name('client.')->
     Route::post('/my-website/{website}/update', [EditWebsiteController::class, 'update'])->name('myWebsite.update');
     Route::patch('/my-website/{website}/activate', [EditWebsiteController::class, 'activateWebsite'])->name('myWebsite.activate');
 
+    // Verify Email
+    Route::post('/my-website/{website}/verify-email', [VerifyWebsiteEmailController::class, 'verify'])->name('myWebsite.verifyEmail');
+    Route::post('/my-website/{website}/verify-email/send-otp', [VerifyWebsiteEmailController::class, 'sendOtp'])->name('myWebsite.verifyEmail.sendOtp');
+    Route::post('/my-website/{website}/verify-email/check-last-otp', [VerifyWebsiteEmailController::class, 'checkLastOtp'])->name('myWebsite.verifyEmail.checkLastOtp');
+
     // Delete
     Route::delete('/my-website/{website}/destroy', [DeleteWebsiteController::class, 'destroy'])->name('myWebsite.destroy');
-    Route::post('/my-website/{website}/sendOtp', [DeleteWebsiteController::class, 'sendOtp'])->name('myWebsite.sendOtp');
-    Route::post('/my-website/{website}/checkLastOtp', [DeleteWebsiteController::class, 'checkLastOtp'])->name('myWebsite.checkLastOtp');
+    Route::post('/my-website/{website}/destroy/send-otp', [DeleteWebsiteController::class, 'sendOtp'])->name('myWebsite.destroy.sendOtp');
+    Route::post('/my-website/{website}/destroy/check-last-otp', [DeleteWebsiteController::class, 'checkLastOtp'])->name('myWebsite.destroy.checkLastOtp');
 
     // UI
     Route::get('/my-website/{website}/ui', [UiWebsiteController::class, 'index'])->name('myWebsite.ui');
-    Route::patch('/my-website/{website}/ui/toggleActive', [UiWebsiteController::class, 'toggleActive'])->name('myWebsite.ui.toggleActive');
+    Route::patch('/my-website/{website}/ui/toggle-active', [UiWebsiteController::class, 'toggleActive'])->name('myWebsite.ui.toggleActive');
     Route::post('/my-website/{website}/ui/create', [UiWebsiteController::class, 'create'])->name('myWebsite.ui.create');
     Route::delete('/my-website/{website}/ui/destroy', [UiWebsiteController::class, 'destroy'])->name('myWebsite.ui.destroy');
 
