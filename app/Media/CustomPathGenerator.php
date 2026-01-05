db7308c1c5e5634014d1b4b287aad49266d0d2b8
rafeaaSaayfan <rafehsaayfan@gmail.com>
2026-01-05 21:05:50 +0200

    Add CustomPathGenerator for media path management
    - Introduced a new CustomPathGenerator class to define custom paths for media storage, enhancing organization and retrieval.
    - Implemented methods for generating paths for media, conversions, and responsive images, improving flexibility in media handling.
    - Integrated logic to create unique base paths based on media model types, ensuring proper categorization of media assets.

---
+++ app/Media/CustomPathGenerator.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Media;
+
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\Support\PathGenerator\PathGenerator;
+
+class CustomPathGenerator implements PathGenerator
+{
+    /*
+     * Get the path for the given media, relative to the root storage path.
+     */
+    public function getPath(Media $media): string
+    {
+        return $this->getBasePath($media).'/';
+    }
+
+    /*
+     * Get the path for conversions of the given media, relative to the root storage path.
+     */
+    public function getPathForConversions(Media $media): string
+    {
+        return $this->getBasePath($media).'/conversions/';
+    }
+
+    /*
+     * Get the path for responsive images of the given media, relative to the root storage path.
+     */
+    public function getPathForResponsiveImages(Media $media): string
+    {
+        return $this->getBasePath($media).'/responsive-images/';
+    }
+
+    /*
+     * Get a unique base path for the given media.
+     */
+    protected function getBasePath(Media $media): string
+    {
+        $prefix = config('media-library.prefix', '');
+
+        if ($media->model_type === \App\Models\Website::class) {
+            $prefix = "websites/{$media->model_id}";
+
+        } elseif ($media->model_type === \App\Models\EcommerceProductVariant::class) {
+            $product = \App\Models\EcommerceProductVariant::find($media->model_id);
+
+            if ($product) {
+                $prefix = "websites/{$product->website_id}/products/{$product->id}";
+            }
+        }
+
+        if ($prefix !== '') {
+            return $prefix.'/'.$media->getKey();
+        }
+
+        return $media->getKey();
+    }
+}
