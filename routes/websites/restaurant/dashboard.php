4d8e51dac1327d8b2bb85803355f5f8db6e33e7c
rafeaaSaayfan <rafehsaayfan@gmail.com>
2026-01-15 23:34:06 +0200

    Refactor restaurant dashboard routes to include Menu Items, Orders, Track Orders, and Carts
    - Added routes for MenuItems, Orders, TrackOrders, and Carts to the restaurant dashboard.
    - Implemented patch routes for managing the active status, home visibility, special status, and discounts for menu items and orders.
    - Established patch route for cart status management, enhancing the functionality of the restaurant dashboard.

--- routes/websites/restaurant/dashboard.php
+++ routes/websites/restaurant/dashboard.php
@@ -3,29 +3,33 @@
 // use App\Http\Controllers\Websites\Ecommerce\Dashboard\Pages\DeliveryFeesController;
 
 use App\Http\Controllers\Websites\Restaurant\Dashboard\DashboardController;
+use App\Http\Controllers\Websites\Restaurant\Dashboard\Pages\CartsController;
+use App\Http\Controllers\Websites\Restaurant\Dashboard\Pages\MenuItemsController;
+use App\Http\Controllers\Websites\Restaurant\Dashboard\Pages\OrdersController;
+use App\Http\Controllers\Websites\Restaurant\Dashboard\Pages\TrackOrdersController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('dashboard')->name('dashboard.')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('index');
 
-    // //* Products
-    // dashboardPagesRoutes('products', ProductsController::class);
-    // Route::patch('products/{id}/is_active', [ProductsController::class, 'toggleActive'])->name('products.is_active');
-    // Route::patch('products/{id}/is_in_home', [ProductsController::class, 'toggleIsInHome'])->name('products.is_in_home');
-    // Route::patch('products/{id}/is_special', [ProductsController::class, 'toggleIsSpecial'])->name('products.is_special');
-    // Route::patch('products/{id}/is_discount', [ProductsController::class, 'toggleIsDiscount'])->name('products.is_discount');
+    //* Menu Items
+    dashboardPagesRoutes('menu-items', MenuItemsController::class);
+    Route::patch('menu-items/{id}/is_active', [MenuItemsController::class, 'toggleActive'])->name('menuItems.is_active');
+    Route::patch('menu-items/{id}/is_in_home', [MenuItemsController::class, 'toggleIsInHome'])->name('menuItems.is_in_home');
+    Route::patch('menu-items/{id}/is_special', [MenuItemsController::class, 'toggleIsSpecial'])->name('menuItems.is_special');
+    Route::patch('menu-items/{id}/is_discount', [MenuItemsController::class, 'toggleIsDiscount'])->name('menuItems.is_discount');
 
     // //* Delivery Fees
     // // dashboardPagesRoutes('delivery-fees', DeliveryFeesController::class);
 
-    // //* Orders
-    // dashboardPagesRoutes('orders', OrdersController::class);
-    // Route::patch('orders/{id}/status', [OrdersController::class, 'changeStatus'])->name('orders.status');
+    //* Orders
+    dashboardPagesRoutes('orders', OrdersController::class);
+    Route::patch('orders/{id}/status', [OrdersController::class, 'changeStatus'])->name('orders.status');
 
-    // //* Track Orders
-    // dashboardPagesRoutes('track-orders', TrackOrdersController::class);
+    //* Track Orders
+    dashboardPagesRoutes('track-orders', TrackOrdersController::class);
 
-    // //* Carts
-    // dashboardPagesRoutes('carts', CartsController::class);
-    // Route::patch('carts/{id}/status', [CartsController::class, 'changeStatus'])->name('carts.status');
+    //* Carts
+    dashboardPagesRoutes('carts', CartsController::class);
+    Route::patch('carts/{id}/status', [CartsController::class, 'changeStatus'])->name('carts.status');
 });
