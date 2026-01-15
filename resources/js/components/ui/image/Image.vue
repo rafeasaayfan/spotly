3a0b4dc4dfae1748db3eb6be1d6fa337ac1a57f1
rafeaaSaayfan <rafehsaayfan@gmail.com>
2026-01-15 23:41:58 +0200

    Enhance Image component with teleport functionality and body overflow management
    - Updated Image.vue to include a teleport feature for fullscreen image previews.
    - Added a watch effect to manage body overflow when the image modal is displayed.
    - Introduced a default value for the withTeleport prop to improve usability.

--- resources/js/components/ui/image/Image.vue
+++ resources/js/components/ui/image/Image.vue
@@ -1,18 +1,30 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
 import { cn } from '@/lib/utils'
+import { watch } from 'vue';
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   src?: string;
   alt?: string;
   class?: string;
-}>();
+  withTeleport?: boolean
+}>(), {
+  withTeleport: false
+});
 
 // Modal toggle
 const show = ref(false);
 
 // Fallback image
 const imageSrc = computed(() => props.src || '/images/default-image.avif');
+
+watch(show, (value) => {
+  if (value) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+});
 </script>
 
 <template>
@@ -26,10 +38,27 @@ const imageSrc = computed(() => props.src || '/images/default-image.avif');
     />
 
     <!-- Fullscreen Preview -->
-    <Transition name="fade">
+    <Teleport v-if="props.withTeleport" to="body">
+      <Transition name="fade">
+        <div
+          v-if="show"
+          class="fixed inset-0 bg-black bg-opacity-80 p-5 flex items-center justify-center z-[9999]"
+          @click.self="show = false"
+        >
+          <img
+            :src="imageSrc"
+            :alt="props.alt || 'Full image'"
+            class="max-w-full max-h-full rounded-lg cursor-pointer"
+            @click="show = false"
+          />
+        </div>
+      </Transition>
+    </Teleport>
+
+    <Transition v-else name="fade">
       <div
         v-if="show"
-        class="fixed inset-0 bg-black bg-opacity-80 p-5 flex items-center justify-center z-50"
+        class="fixed inset-0 bg-black bg-opacity-80 p-5 flex items-center justify-center z-[9999]"
         @click.self="show = false"
       >
         <img
